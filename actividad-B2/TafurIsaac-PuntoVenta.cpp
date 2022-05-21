0601c0927dabb1d00e7336109477a46ac2025a7a
leontafur <isaac.tafur.leon@utelvt.edu.ec>
2022-05-20 23:36:03 -0500

    Actualizacion de los diagramas y programas que cree en la actividad pasada

--- actividad-B2/TafurIsaac-PuntoVenta.cpp
+++ actividad-B2/TafurIsaac-PuntoVenta.cpp
@@ -1,42 +1,57 @@
-#include <iostream>
+//================================================
+//==> Nombre del programa: Factura
+//==> Archivo : ReneBalseca_PuntoVenta.cpp
+//==>Autor: Carlos Rene Balseca Nevarez
+//==>Fecha de elaboración: 2022-03-20
+//==>Fecha ultima actualización: 2022-05-18
+//=================================================
 
+#include <iostream>
 using namespace std;
 
 int main(){
 
-	int i=0;
-	float num,des,iva,subt,t,total;
+	int IS_i=0;
+	float IS_num,IS_des,IS_iva,IS_subt,IS_t,IS_total;
 
-	cout<<"cual es la cantidad de productos que vas a reregistrar para tu factura"<<endl;
-	cin>>i;
+	cout<<"cuantos productos vas a reregistrar para tu factura"<<endl;
+	cin>>IS_i;
 
 	do {
 		
 	cout<< "cual es el valor de los productos:"<<endl;
-		cin>>num;
-		t=num+t;
-		i--;
+		cin>>IS_num;
+		IS_t=IS_num+IS_t;
+		IS_i--;
 
-		}while(i>0);	
+		}while(IS_i>0);	
 
 
 		cout<<"porsentaje del descueno"<<endl;
-		cin>>des;
-	if (des>1){
-		des=t*des/100;
-		subt=t-des;
+		cin>>IS_des;
+	if (IS_des>1){
+		IS_des=IS_t*IS_des/100;
+		IS_subt=IS_t-IS_des;
 	}
 	else{
-		subt=t;
+		IS_subt=IS_t;
 	}
 
-	iva=subt*12/100;
-	total=subt+iva;
+	IS_iva=IS_subt*12/100;
+	IS_total=IS_subt+IS_iva;
 	
-	cout<< "el descuento es de: $"<<des<<endl;
-	cout<<"el subtotal es: $"<<subt<<endl;
-	cout<<"el iva es: $"<<iva<<endl;
-	cout<< "total de la factura es: $"<<total<<endl;
+	cout<<"el subtotal es: $"<<IS_subt<<endl;
+	cout<< "el descuento es de: $"<<IS_des<<endl;
+	cout<<"el iva es: $"<<IS_iva<<endl;
+	cout<< "total de la factura es: $"<<IS_total<<endl;
+	
+cout<<"//================================================"<<endl;
+cout<<"//==>Nombre del programa: factura"<<endl;
+cout<<"//==>Archivo : TafurIsaac-PuntoVenta.cpp"<<endl;
+cout<<"//==>Autor: Isaac Alejandro Tafur León"<<endl;
+cout<<"//==>Fecha de elaboracion: 2022-03-20"<<endl;
+cout<<"//==>Fecha ultima actualizacion: 2022-05-18"<<endl;
+cout<<"//=================================================";
 
 	return 0;
 }

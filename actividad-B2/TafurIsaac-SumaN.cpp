0646eae68a20d6f6d9ff351927725f3b7afcb900
leontafur <isaac.tafur.leon@utelvt.edu.ec>
2022-05-21 00:01:16 -0500

    Actualizacion de los diagramas y programas que cree en la actividad pasada

--- actividad-B2/TafurIsaac-SumaN.cpp
+++ actividad-B2/TafurIsaac-SumaN.cpp
@@ -1,19 +1,36 @@
+//================================================
+//==> Nombre del programa: Suma de varios numeros
+//==> Archivo : TafurIsaac-SumaN.cpp
+//==>Autor: Isaac Alejandro Tafur León
+//==>Fecha de elaboración: 2022-03-20
+//==>Fecha ultima actualización: 2022-05-18
+//=================================================
 #include<iostream>
 using namespace std;
 
-int main()
-{
-	int i=0,A;
-	float l=0,t;
-	cout<<"Ingrese la cantidad de valor a sumar : ";
-	cin>>A;
-	do{
-	cout<<"Ingrese el elemento "<<i+1;
-		cin>>t;
-		i=i+1;
-		l=l+t;
-	}while (i<A);
-	cout<<"La suma de los elementos fue : "<<l<<endl;
+int main(){
+	
+	int IS_i=0,IS_a,IS_s;
+	cout<<"cuantos numeros va a sumar"<<endl;
+	cin>>IS_i;
 
-	return(0);
+	do {
+		
+	cout<< "cual es el numero q vas a sumar:"<<endl;
+		cin>>IS_a;
+		IS_s=IS_s+IS_a;
+		IS_i--;
+	}while(IS_i>0);
+		
+	cout<<"el resultado de la suma es: " <<IS_s<<endl;
+	
+cout<<"//================================================"<<endl;
+cout<<"//==>Nombre del programa: La comparacion de 2 numeros"<<endl;
+cout<<"//==>Archivo : TafurIsaac-SumaN.cpp"<<endl;
+cout<<"//==>Autor: Isaac Alejandro Tafur León"<<endl;
+cout<<"//==>Fecha de elaboracion: 2022-03-20"<<endl;
+cout<<"//==>Fecha ultima actualizacion: 2022-05-18"<<endl;
+cout<<"//=================================================";
+	
+	return 0;
 }

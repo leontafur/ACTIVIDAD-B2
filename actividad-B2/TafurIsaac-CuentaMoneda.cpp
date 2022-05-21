a270fa18667678c4edf4b8ab0bab50ba93a40013
leontafur <isaac.tafur.leon@utelvt.edu.ec>
2022-05-20 21:59:36 -0500

    Actualizacion de los diagramas y programas que cree en la actividad pasada

--- actividad-B2/TafurIsaac-CuentaMoneda.cpp
+++ actividad-B2/TafurIsaac-CuentaMoneda.cpp
@@ -1,26 +1,42 @@
+//================================================
+//==> Nombre del programa: Cuenta de monedas
+//==> Archivo : TafurIsaac-CuentaMoneda.cpp
+//==>Autor: Isaac Alejandro Tafur León
+//==>Fecha de elaboración: 2022-03-20
+//==>Fecha ultima actualización: 2022-05-18
+//=================================================
+
 #include<iostream>
 using namespace std;
 int main(){
 	
-	int di,ve,ci;
-	float diez,vein,ciq,total;
+	int IS_di,IS_ve,IS_ci;
+	float IS_diez,IS_vein,IS_ciq,IS_total;
 	
 	cout<<"cuantas monedas de 10ctv son"<<endl;
-	cin>>di;
+	cin>>IS_di;
 	cout<<"cuantas monedas de 25ctv son"<<endl;
-	cin>>ve;
+	cin>>IS_ve;
 	cout<<"cuantas monedas de 50ctv son"<<endl;
-	cin>>ci;
+	cin>>IS_ci;
 	
-	diez=di*0.10;
-	vein=ve*0.25;
-	ciq=ci*0.50;
-	total=diez+vein+ciq;
+	IS_diez=IS_di*0.10;
+	IS_vein=IS_ve*0.25;
+	IS_ciq=IS_ci*0.50;
+	IS_total=IS_diez+IS_vein+IS_ciq;
 	
-	cout<<"Tolal de las monedas de 10ctv es: $"<<diez<<endl;
-	cout<<"Tolal de las monedas de 25ctv es: $"<<vein<<endl;
-	cout<<"Tolal de las monedas de 50ctv es: $"<<ciq<<endl;
-	cout<<"EL valor total es: $"<<total;
+	cout<<"el tolal de las monedas de 10ctv es: $"<<IS_diez<<endl;
+	cout<<"el tolal de las monedas de 25ctv es: $"<<IS_vein<<endl;
+	cout<<"el tolal de las monedas de 50ctv es: $"<<IS_ciq<<endl;
+	cout<<"el valor total es: $"<<IS_total<<endl;
+	
+cout<<"//================================================"<<endl;
+cout<<"//==>Nombre del programa: Cuenta monedas"<<endl;
+cout<<"//==>Archivo : TafurIsaac-CuentaMoneda.cpp"<<endl;
+cout<<"//==>Autor: Isaac Alejandro Tafur León"<<endl;
+cout<<"//==>Fecha de elaboracion: 2022-03-20"<<endl;
+cout<<"//==>Fecha ultima actualizacion: 2022-05-18"<<endl;
+cout<<"//=================================================";
 	
 	return 0;
 }

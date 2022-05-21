bc5393624c045a9bc728854b65d46bb0d84d2a83
leontafur <isaac.tafur.leon@utelvt.edu.ec>
2022-05-20 22:53:10 -0500

    Actualizacion de los diagramas y programas que cree en la actividad pasada

--- actividad-B2/TafurIsaac-Laedad.cpp
+++ actividad-B2/TafurIsaac-Laedad.cpp
@@ -1,40 +1,59 @@
+//================================================
+//==> Nombre del programa: La edad de una persona
+//==> Archivo : TafurIsaac-Laedad.cpp
+//==>Autor: Isaac Alejandro Tafur León
+//==>Fecha de elaboración: 2022-03-20
+//==>Fecha ultima actualización: 2022-05-18
+//=================================================
+
 #include<iostream>
 using namespace std;
 
 int main(){
-	int aa,an,da,dn,edad,ma,mn;
-	cout<<"ingrese su fecha de nacimiento"<<endl;
-	cout<<"formato de dia, mes, año"<<endl;
-	cin>> dn;
-	cin>> mn;
-	cin>> an;
+	int IS_aa,IS_an,IS_da,IS_dn,IS_edad,IS_ma,IS_mn;
 	cout<<"ingrese su fecha actual"<<endl;
 	cout<<"formato de dia, mes, año"<<endl;
-	cin>> da;
-	cin>> ma;
-	cin>> aa;
-	edad=aa-an;
-	if(ma<mn){
-		edad=edad-1;
+		cin>> IS_da;
+		cin>> IS_ma;
+		cin>> IS_aa;	
+	cout<<"ingrese su fecha de nacimiento"<<endl;
+	cout<<"formato de dia, mes, año"<<endl;
+		cin>> IS_dn;
+		cin>> IS_mn;
+		cin>> IS_an;
+	
+	IS_edad=IS_aa-IS_an;
+	
+	if(IS_ma<IS_mn){
+		IS_edad=IS_edad-1;
 	}
 	else{
-		if(ma==mn){
-			if(da<dn)
-			edad=edad-1;
+		if(IS_ma==IS_mn){
+			if(IS_da<IS_dn)
+			IS_edad=IS_edad-1;
 			
 		}
 		else{
-			if(dn==da){
-				cout<<"felicidades hoy es tu cumpleaños"<<endl;
+			if(IS_dn==IS_da){
+				cout<<"felicidades es tu cumpleaños"<<endl;
 			}
 		}
 	}
-	if(edad<0){
-		cout<<"Lo siento no se puede carcular la edad";
+	if(IS_edad<0){
+		cout<<"no se puede carcular la edad";
 	}
 		else{
-			cout<<"tu edad es: "<<edad;
+			IS_edad=IS_edad-1;
+			cout<<"tu edad es: "<<IS_edad<<endl;
 		}
-	
+
+cout<<"//================================================"<<endl;
+cout<<"//==>Nombre del programa: la edad de una persona"<<endl;
+cout<<"//==>Archivo : TafurIsaac-Laedad.cpp"<<endl;
+cout<<"//==>Autor: Isaac Alejandro Tafur León"<<endl;
+cout<<"//==>Fecha de elaboracion: 2022-03-20"<<endl;
+cout<<"//==>Fecha ultima actualizacion: 2022-05-18"<<endl;
+cout<<"//=================================================";
+
 	return 0;
 }

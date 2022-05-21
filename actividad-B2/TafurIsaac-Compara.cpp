8346affcf8be29b0c89ca94dc0d5652ef91864f3
leontafur <isaac.tafur.leon@utelvt.edu.ec>
2022-05-20 21:08:41 -0500

    Actualizacion de los diagramad y programas que cree en la actividad pasada

--- actividad-B2/TafurIsaac-Compara.cpp
+++ actividad-B2/TafurIsaac-Compara.cpp
@@ -1,27 +1,42 @@
-#include<iostream>
+//================================================
+//==> Nombre del programa: La comparacion de 2 numeros
+//==> Archivo : TafurIsaac-Compara.cpp
+//==>Autor: Isaac Alejandro Tafur León
+//==>Fecha de elaboración: 2022-03-20
+//==>Fecha ultima actualización: 2022-05-18
+//=================================================
+
+#include <iostream>
 using namespace std;
-int main()
-{
-	float x,y;
-	cout<<"Ingresar x: "; cin>>x;
-	cout<<"Ingresar y: "; cin>>y;
 
-	if(x==y){
-	cout<<"El valor de x= " <<x<<" es igual a y= "<<y<<endl;
-
-	}else{
-
-	if(x<y){
-	cout<<"El valor de x= " <<x<<" ES MENOR A y= "<<y<<endl;
-
-	}else{
+int main(){
+	int IS_a,IS_b; 
+ 
+	cout<< "El numero que vas a comparar"<<endl;
+	cin>>IS_a;
+	cin>>IS_b;
+	if(IS_a==IS_b){
+	cout<<"Ambos numeros son iguales"<<endl;
+	}
+		
+	else{
+		if(IS_a<IS_b){
+			cout<<"El numero mayor es: "<<IS_b<<endl;
+			cout<<"El numero menor es: "<<IS_a<<endl;
+		}
+		else{
+			cout<<"El numero mayor es :"<<IS_a<<endl;
+			cout<<"El numero menor es :"<<IS_b<<endl;
+		}	
+	}
 	
-	cout<<"el valor de y= " <<y<<" ES MENOR A x= "<<x<<endl;
+cout<<"//================================================"<<endl;
+cout<<"//==>Nombre del programa: La comparacion de 2 numeros"<<endl;
+cout<<"//==>Archivo : TafurIsaac-Compara.cpp"<<endl;
+cout<<"//==>Autor: Isaac Alejandro Tafur León"<<endl;
+cout<<"//==>Fecha de elaboracion: 2022-03-20"<<endl;
+cout<<"//==>Fecha ultima actualizacion: 2022-05-18"<<endl;
+cout<<"//=================================================";
 
+	return 0;
 	}
-
-	}
-
-return 0;
-
-}
